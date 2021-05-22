ce1c810cb6fcdaeaf1c29fe084cc8dc09e7b3b98
Guillesilvaf <84109507+Guillesilvaf@users.noreply.github.com>
2021-05-22 11:25:48 -0300

    Menu en pantalla principal

--- ObligatorioCódigo/js/obligatorio.js
+++ ObligatorioCódigo/js/obligatorio.js
@@ -13,7 +13,6 @@ function inicio(){
     document.querySelector("#btnVentanaRegistrar").addEventListener("click", verVentanaRegistrar);    
     document.querySelector("#btnVerAsignarNivel").addEventListener("click", verVentanaAsignarNivel);   
     document.querySelector("#btnAsignarNivel").addEventListener("click", asignarNivel);
-    document.querySelector("#btnMostrarNiveles").addEventListener("click", mostrarNiveles);
 
     document.querySelector("#btnVerEjerciciosPlanteados").addEventListener("click", cargarEjercicios);
     
@@ -215,6 +214,24 @@ function verVentanaRegistrar(){
     ocultar("#divIngreso");
 }
 
+function cargarMenu(tipoUsuario){
+let menuAmostrar = "";
+
+    if(elemento.tipo==="A"){
+        menuAmostrar = '<li> <a>'+"Ver ejercicios planteados y entregar"+'</a> </li>';
+        menuAmostrar+= '<li> <a>'+"Ver ejercicios resueltos"+'</a> </li>';
+        menuAmostrar+= '<li> <a>'+"Informacion estadistica"+'</a> </li>';         
+    }else{
+        menuAmostrar = '<li> <a>'+"Asignar nivel alumno"+'</a> </li>';
+        menuAmostrar+= '<li> <a>'+"Plantear ejercicios"+'</a> </li>';
+        menuAmostrar+= '<li> <a>'+"Realizar devoluciones"+'</a> </li>'; 
+        menuAmostrar+= '<li> <a>'+"Informacion estadistica"+'</a> </li>';
+      
+    }
+    menuAmostrar+= '<li> <a>'+"Salir"+'</a> </li>';
+    document.querySelector("#navPrincipal").innerHTML= menuAmostrar;
+}
+
 //------------------------------------------------------------------------------------------------------------------------------//
 //------------------------------------------------------------------------------------------------------------------------------//
 //--------------------------------------------------MENU DOCENTE----------------------------------------------------------------//
@@ -234,7 +251,7 @@ function verVentanaAsignarNivel(){
 }
 
 function mostrarNiveles(){
-    mostrar("#divMostrarNiveles")
+    mostrar("#divNivelesAsignar")
     let id_alumno_seleccionado = document.getElementById("regAlumnos").value;
 
     let niveles_a_mostrar=""; 
@@ -254,7 +271,7 @@ function mostrarNiveles(){
                      break;
 
                  case '3':
-                     ocultar("#divMostrarNiveles")
+                     ocultar("#divNivelesAsignar")
                      alert("El nivel del alumno es Avanzado, no se permite reducir el nivel del mismo");
                         break;
                  
